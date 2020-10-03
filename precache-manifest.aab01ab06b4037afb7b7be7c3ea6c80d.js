44011800217119354620d1b218e7b951abcc13b5
Amund Berger Lindstøl <amund.lindstol@MacBook-Pro.local>
2020-10-03 13:56:11 +0200

    Updates

---
+++ precache-manifest.aab01ab06b4037afb7b7be7c3ea6c80d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bce5dabe58f7c5aeec9a53984c694b",
+    "url": "/kaffi/index.html"
+  },
+  {
+    "revision": "d6aa146ecf8fc268fffd",
+    "url": "/kaffi/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "0eff13a3d31047c022ef",
+    "url": "/kaffi/static/css/main.32884815.chunk.css"
+  },
+  {
+    "revision": "d6aa146ecf8fc268fffd",
+    "url": "/kaffi/static/js/2.69dde09d.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/kaffi/static/js/2.69dde09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eff13a3d31047c022ef",
+    "url": "/kaffi/static/js/main.c66ae08b.chunk.js"
+  },
+  {
+    "revision": "7103b54a7cd47876529b",
+    "url": "/kaffi/static/js/runtime-main.8d183233.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaffi/static/media/logo.5d5d9eef.svg"
+  }
+]);
